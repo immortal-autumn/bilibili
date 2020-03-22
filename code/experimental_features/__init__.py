14d38be34b318cdcbaa03839660cb520324ffe1f
iydon <11711217@mail.sustech.edu.cn>
2020-03-22 21:09:29 +0800

    update: draft of experimental features: LiveByRoom

---
+++ code/experimental_features/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+__all__ = ('LiveByArea', )
+
+
+import warnings
+
+from .model import LiveByArea
+
+
+warnings.warn('This is experimental features, which is not stable.',
+    DeprecationWarning, stacklevel=2)
--- code/main.py
+++ code/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from time import sleep
 from selenium import webdriver
 
 from bilibili.model import Auto
+
+from experimental_features import LiveByArea
 
 
 if __name__ == '__main__':
@@ -9,4 +12,20 @@ if __name__ == '__main__':
     ## Cache the webdriver
     if 'browser' not in locals():
         browser = webdriver.Chrome()
-    a = Auto(web_driver=browser, login=True)
+    # a = Auto(web_driver=browser, login=True)
+
+    # 直播间流言
+    input('Please login >>> ')
+    live = LiveByArea(27)
+    for url in live.urls:
+        browser.get(url)
+        sleep(5)
+        browser.find_element_by_class_name('chat-input.border-box') \
+            .send_keys('串门～今天有没有好好学习呀，一起加油')
+        sleep(3)
+        try:
+            browser.find_element_by_class_name('txt') \
+                .click()
+        except:
+            continue
+        sleep(3)
